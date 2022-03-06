19fd915a0a00d98d8a778b5c119030578a773eba
lakhdar-195 <lakhdar.developpeur@gmail.com>
2022-03-06 18:35:57 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+// require app BrowserWindow
+const { app, BrowserWindow, Menu, MenuItem } = require('electron');
+
+// create window 
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600
+    })
+  
+    win.loadFile('index.html')
+  }
+
+//launch window 
+app.whenReady().then(() => {
+  createWindow()
+})
+
+// custom menu 
+const menu = new Menu()
+menu.append(new MenuItem({
+    label: 'File',
+    submenu:[    
+                {
+                    role: 'help',
+                    accelerator: process.platform === 'darwin' ? 'Alt+Cmd+I' : 'Alt+Shift+I',
+                    click: () => { console.log('Electron rocks!') }
+                },
+                {
+                    role: 'Quit',
+                    accelerator: process.platform === 'darwin' ? 'Cmd+Q' : 'Ctrl+Q',
+                    click: () => {app.quit()}
+
+                }
+    
+            ]
+}))
+
+Menu.setApplicationMenu(menu)
+
+// create close window
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  })
+
