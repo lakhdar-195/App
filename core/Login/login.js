7645781122e1f8834ef4ac75287810e9490c5fda
lakhdar-195 <lakhdar.developpeur@gmail.com>
2022-03-07 22:44:51 +0100

    comment login

--- core/Login/login.js
+++ core/Login/login.js
@@ -2,9 +2,11 @@ var sha1 = require('sha1')
 
 function Login ()  {
 
+    // init email and password
     var email = "";
     var password = "";
 
+    // get email and password
     this.init = function () {
 
         $("#submit").click(function () {
@@ -12,26 +14,31 @@ function Login ()  {
             email = $('#email').val();
             password = $('#password').val();
 
-            
             log();
         });
 
     }
 
+    
     function log () {
 
+        // hash sha1 password
         var $password = sha1(password);
-        console.log($password);
 
+        // sql req
         var sql = "SELECT * FROM Login WHERE email='" + email + "' AND password='" + $password + "'";
+
+        //connect mysql
         connection.connect();
  
+        // query connection
         connection.query(sql, function (error, results) {
             if (error) throw error;
             console.log('The solution is: ', results[0]);
 
             if(results[0]){
 
+                // send to home page
                 $router.menu();
             }
         });
